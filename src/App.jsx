4e63d36f818b39994a93aeb89299f046751cccc6
Susobhan Das <susobhandas009@gmail.com>
2022-12-30 18:25:29 +0530

    Solved the issue of updating answers after submission. Eliminated non-functional variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
-import {nanoid} from 'nanoid';
+import { nanoid } from 'nanoid';
 import Intro from './intro';
 import Questions from './questions';
 
-export default function main(){
-    let [startQuize,setStartQuize]=React.useState(false),qlist;
-    let [questionsLoaded,setQuestionsLoaded]=React.useState(false);
-    function loadQuestions(){
+export default function main() {
+    let [startQuize, setStartQuize] = React.useState(false), qlist;
+    let [questionsLoaded, setQuestionsLoaded] = React.useState(false);
+    function loadQuestions() {
         setStartQuize(true);
     }
-    let [questionSet,setQuestionSet]=React.useState([]);
-    React.useEffect(()=>{
+    let [questionSet, setQuestionSet] = React.useState([]);
+    React.useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=5&type=multiple')
-        .then(res=>res.json())
-        .then(data=>{
-            let questionset=data.results.map((value)=>{
-                Array.prototype.insert = function ( index, ...items ) {
-                    this.splice( index, 0, ...items );
-                };
-                let ans=value.incorrect_answers;
-                ans.insert(Math.round(Math.random()*3),value.correct_answer)
-                return {
-                    id:nanoid(),
-                    question:value.question,
-                    answers:ans,
-                    correct_ans:value.correct_answer,
-                    marked_ans:""
-                }
+            .then(res => res.json())
+            .then(data => {
+                let questionset = data.results.map((value) => {
+                    Array.prototype.insert = function (index, ...items) {
+                        this.splice(index, 0, ...items);
+                    };
+                    let ans = value.incorrect_answers;
+                    ans.insert(Math.round(Math.random() * 3), value.correct_answer)
+                    return {
+                        id: nanoid(),
+                        question: value.question,
+                        answers: ans,
+                        correct_ans: value.correct_answer,
+                        marked_ans: ""
+                    }
+                });
+                setQuestionSet(questionset);
+                setQuestionsLoaded(true);
             });
-            setQuestionSet(questionset);
-            setQuestionsLoaded(true);
-        });
-    },[]);
-    return(
+    }, []);
+    return (
         <div id='appDiv'>
-        {questionsLoaded && (startQuize? <Questions data={questionSet} /> : <Intro loadQuestion={loadQuestions}/>)}
+            {questionsLoaded && (startQuize ? <Questions data={questionSet} /> : <Intro loadQuestion={loadQuestions} />)}
         </div>
     );
 }
--- src/question.jsx
+++ src/question.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
-export default function main(props){
-    let styles=props.answers.map(value=>{
-        return {
-            backgroundColor: props.marked===value ? 'hsla(240,100%,50%,0.5)' : ''
-        }
-    });
-    return(
+export default function main(props) {
+    return (
         <fieldset className='container'>
-        <legend dangerouslySetInnerHTML={{__html:props.question}}/>
-        <input checked={props.marked===props.answers[0]} onChange={(event)=>props.handeler(event,props.id)} type='radio' id={props.answers[0]} name={props.question} value={props.answers[0]}/>
-        <label className={props.id} style={styles[0]}  htmlFor={props.answers[0]} dangerouslySetInnerHTML={{__html:props.answers[0]}} />
-        <br />
-        <input checked={props.marked===props.answers[1]} onChange={(event)=>props.handeler(event,props.id)} type='radio' id={props.answers[1]} name={props.question} value={props.answers[1]}/>
-        <label className={props.id} style={styles[1]}  htmlFor={props.answers[1]} dangerouslySetInnerHTML={{__html:props.answers[1]}} />
-        <br />
-        <input checked={props.marked===props.answers[2]} onChange={(event)=>props.handeler(event,props.id)} type='radio' id={props.answers[2]} name={props.question} value={props.answers[2]}/>
-        <label className={props.id} style={styles[2]}  htmlFor={props.answers[2]} dangerouslySetInnerHTML={{__html:props.answers[2]}} />
-        <br />
-        <input checked={props.marked===props.answers[3]} onChange={(event)=>props.handeler(event,props.id)} type='radio' id={props.answers[3]} name={props.question} value={props.answers[3]}/>
-        <label className={props.id} style={styles[3]}  htmlFor={props.answers[3]} dangerouslySetInnerHTML={{__html:props.answers[3]}} />
+            <legend dangerouslySetInnerHTML={{ __html: props.question }} />
+            <input checked={props.marked === props.answers[0]} onChange={(event) => props.handeler(event, props.id)} type='radio' id={props.answers[0]} name={props.question} value={props.answers[0]} />
+            <label className={props.id} htmlFor={props.answers[0]} dangerouslySetInnerHTML={{ __html: props.answers[0] }} />
+            <br />
+            <input checked={props.marked === props.answers[1]} onChange={(event) => props.handeler(event, props.id)} type='radio' id={props.answers[1]} name={props.question} value={props.answers[1]} />
+            <label className={props.id} htmlFor={props.answers[1]} dangerouslySetInnerHTML={{ __html: props.answers[1] }} />
+            <br />
+            <input checked={props.marked === props.answers[2]} onChange={(event) => props.handeler(event, props.id)} type='radio' id={props.answers[2]} name={props.question} value={props.answers[2]} />
+            <label className={props.id} htmlFor={props.answers[2]} dangerouslySetInnerHTML={{ __html: props.answers[2] }} />
+            <br />
+            <input checked={props.marked === props.answers[3]} onChange={(event) => props.handeler(event, props.id)} type='radio' id={props.answers[3]} name={props.question} value={props.answers[3]} />
+            <label className={props.id} htmlFor={props.answers[3]} dangerouslySetInnerHTML={{ __html: props.answers[3] }} />
         </fieldset>
     );
 }
